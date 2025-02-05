46a478b015c2be955a02c0221177d703366ba5a1
Munkhzorig000918 <m4gii.dev@gmail.com>
2025-02-05 22:55:00 +0800

    analytic

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,24 @@ import '../styles/globals.css'
 import { Provider } from "react-redux"
 
 import { reduxStore } from "../redux/store"
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
+const gtag = process.env.NEXT_PUBLIC_GOOGLE_ID
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', gtag, { page_path: url });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  
   return (
     <Provider store={reduxStore}>
       <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,27 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const gtag = process.env.NEXT_PUBLIC_GOOGLE_ID
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
